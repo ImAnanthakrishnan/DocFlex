e0c17a664478263985a10d7de7d390399a920dec
Anantha krishnan <ananthakrishhh2002@gmail.com>
2024-05-11 14:57:55 +0530

    endpoint correction

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,3 @@
-const APP_URL = import.meta.env.VITE_APP_URL;
-
-export const BASE_URL = APP_URL;
+export const BASE_URL = import.meta.env.VITE_APP_URL;
 
 export const ENDPOINT = 'https://docflex.site'
