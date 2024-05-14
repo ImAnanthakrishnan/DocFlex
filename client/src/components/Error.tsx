a59eab710a0fd4f62ad1b693ee8b772132c1a6f7
Anantha krishnan <ananthakrishhh2002@gmail.com>
2024-05-14 10:46:18 +0530

    logout function for error component

--- client/src/components/Error.tsx
+++ client/src/components/Error.tsx
@@ -1,5 +1,25 @@
+import { useAppDispatch, useAppSelector } from "../app/hooks"
+import {signOut as userSignOut} from '../slices/user/userSlice';
+import {signOut as doctorSignOut} from '../slices/doctor/doctorSlice';
+import { signOut as adminSignOut } from "../slices/admin/adminSlice";
 
 const Error = ({errorMessage}:any) => {
+  let {currentUser} = useAppSelector(state=>state.user);
+  let {currentDoctor} = useAppSelector(state=>state.doctor);
+  let {currentAdmin} = useAppSelector(state=>state.admin);
+  
+  let dispatch = useAppDispatch();
+
+  const handleSignout = () => {
+    if(currentUser){
+      dispatch(userSignOut());
+    }else if(currentDoctor){
+      dispatch(doctorSignOut());
+    }else if(currentAdmin){
+      dispatch(adminSignOut());
+    }
+  }
+
   return (
     <div>
       return(
@@ -7,6 +27,7 @@ const Error = ({errorMessage}:any) => {
             <h3 className="text-headingColor text-[20px] leading-[30px] font-semibold">
                 {errorMessage}
             </h3>
+            <button className="btn bg-irisBlueColor rounded-md mt-3" onClick={handleSignout}>Logout</button>
         </div>
       )
     </div>
