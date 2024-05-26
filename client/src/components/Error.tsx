8f94a706202cf5954190c99fbc12dc709fd9ae92
Anantha krishnan <ananthakrishhh2002@gmail.com>
2024-05-26 15:33:01 +0530

    changes

--- client/src/components/Error.tsx
+++ client/src/components/Error.tsx
@@ -1,37 +1,41 @@
-import { useAppDispatch, useAppSelector } from "../app/hooks"
-import {signOut as userSignOut} from '../slices/user/userSlice';
-import {signOut as doctorSignOut} from '../slices/doctor/doctorSlice';
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { signOut as userSignOut } from "../slices/user/userSlice";
+import { signOut as doctorSignOut } from "../slices/doctor/doctorSlice";
 import { signOut as adminSignOut } from "../slices/admin/adminSlice";
 
-const Error = ({errorMessage}:any) => {
-  let {currentUser} = useAppSelector(state=>state.user);
-  let {currentDoctor} = useAppSelector(state=>state.doctor);
-  let {currentAdmin} = useAppSelector(state=>state.admin);
-  
+const Error = ({ errorMessage }: any) => {
+  let { currentUser } = useAppSelector((state) => state.user);
+  let { currentDoctor } = useAppSelector((state) => state.doctor);
+  let { currentAdmin } = useAppSelector((state) => state.admin);
+
   let dispatch = useAppDispatch();
 
   const handleSignout = () => {
-    if(currentUser){
+    if (currentUser) {
       dispatch(userSignOut());
-    }else if(currentDoctor){
+    } else if (currentDoctor) {
       dispatch(doctorSignOut());
-    }else if(currentAdmin){
+    } else if (currentAdmin) {
       dispatch(adminSignOut());
     }
-  }
+  };
 
   return (
     <div>
-      return(
-        <div className="flex items-center justify-center w-full h-full">
-            <h3 className="text-headingColor text-[20px] leading-[30px] font-semibold">
-                {errorMessage}
-            </h3>
-            <button className="btn bg-irisBlueColor rounded-md mt-3" onClick={handleSignout}>Logout</button>
-        </div>
-      )
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <h3 className="text-headingColor text-[20px] leading-[30px] font-semibold">
+          {errorMessage}
+        </h3>
+        <br />
+        <button
+          className="btn bg-irisBlueColor rounded-md mt-4"
+          onClick={handleSignout}
+        >
+          Logout
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
